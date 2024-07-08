6ca2f771b3d5c4f0c9fc5fe50997de894d245b57
DePacos <depacos@yandex.ru>
2024-07-08 23:05:34 +0300

    add save and load store

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,5 +1,6 @@
-import {combineReducers, legacy_createStore} from "redux";
+import { combineReducers, legacy_createStore} from "redux";
 import {counterReducer} from "./reducers/counterReducer.ts";
+import {loadState, saveState} from "../utils/localStorage.ts";
 
 
 
@@ -7,7 +8,10 @@ export const rootReducer = combineReducers({
     counter: counterReducer,
 })
 
-export const store = legacy_createStore(rootReducer)
+export const store = legacy_createStore(rootReducer, loadState('stateCounter'))
 
+store.subscribe(()=>{
+    saveState({counter: store.getState().counter}, 'stateCounter')
+})
 // @ts-ignore
 window.store = store
