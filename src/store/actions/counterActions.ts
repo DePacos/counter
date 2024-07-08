639f2ea5f7b7629d2213b47fe01755a4c976a34a
DePacos <depacos@yandex.ru>
2024-07-08 23:15:36 +0300

    refactor

--- src/store/actions/counterActions.ts
+++ src/store/actions/counterActions.ts
@@ -1,30 +1,30 @@
 import {
-    changeCounterActionType,
-    changeInputMaximumActionType,
-    changeInputStartActionType,
-    toggleErrorActionType
+    ChangeCounterActionType,
+    ChangeInputMaximumActionType,
+    ChangeInputStartActionType,
+    ToggleErrorActionType
 } from "../../types/counter.ts";
 
-export const changeInputStartAC = (value: string):changeInputStartActionType => {
+export const changeInputStartAC = (value: string):ChangeInputStartActionType => {
      return {
          type: 'CHANGE_INPUT_START',
          value
      }
 }
-export const changeInputMaximumAC = (value: string):changeInputMaximumActionType => {
+export const changeInputMaximumAC = (value: string):ChangeInputMaximumActionType => {
      return {
          type: 'CHANGE_INPUT_MAX',
          value
      }
 }
 
-export const toggleErrorAC = (value: boolean):toggleErrorActionType => {
+export const toggleErrorAC = (value: boolean):ToggleErrorActionType => {
     return {
         type: 'TOGGLE_ERROR',
         value
     }
 }
-export const changeCounterAC = (value: string):changeCounterActionType => {
+export const changeCounterAC = (value: string):ChangeCounterActionType => {
     return {
         type: 'CHANGE_COUNTER',
         value
